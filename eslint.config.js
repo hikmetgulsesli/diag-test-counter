f3713c3f18c2061c7976c78c2675ca88107749c7
hikmetgulsesli <hikmetgulsesli@gmail.com>
2026-03-10 13:25:54 +0300

    feat: US-001 - Counter Feature Implementation (#1)
    Co-authored-by: Moltclaw AI <setrox@moltclaw.local>

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
@@ -31,14 +30,12 @@ export default [
       '@typescript-eslint': tseslint,
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'react/react-in-jsx-scope': 'off',
       'no-undef': 'off',
